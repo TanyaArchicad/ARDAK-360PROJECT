550049961fce76c7f97fada29190ed1f06961494
TanyaArchicad <tanyadiz2014@gmail.com>
2024-06-24 13:36:28 +0500

    Upload

--- app-files/data.js
+++ app-files/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-",
-      "name": "Прихожая",
+      "id": "0-prihozaya",
+      "name": "Prihozaya",
       "levels": [
         {
           "tileSize": 256,
@@ -33,26 +33,26 @@ var APP_DATA = {
           "yaw": 2.5997342859486476,
           "pitch": -0.0923239703757357,
           "rotation": 0,
-          "target": "1--1"
+          "target": "1-dyshevaya1"
         },
         {
           "yaw": -2.2889957322824355,
           "pitch": -0.09457354795146067,
           "rotation": 0,
-          "target": "3--1"
+          "target": "3-gostevaya1"
         },
         {
           "yaw": -1.6079152730043411,
           "pitch": 0.02037542687047278,
           "rotation": 0,
-          "target": "5--1"
+          "target": "5-zal1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1--1",
-      "name": "Душевая 1",
+      "id": "1-dyshevaya1",
+      "name": "Dyshevaya1",
       "levels": [
         {
           "tileSize": 256,
@@ -83,14 +83,14 @@ var APP_DATA = {
           "yaw": 2.17524483346107,
           "pitch": 0.09994677523736861,
           "rotation": 0,
-          "target": "2--2"
+          "target": "2-dyshevaya2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2--2",
-      "name": "Душевая 2",
+      "id": "2-dyshevaya2",
+      "name": "Dyshevaya2",
       "levels": [
         {
           "tileSize": 256,
@@ -121,14 +121,14 @@ var APP_DATA = {
           "yaw": 1.7537414866235768,
           "pitch": -0.20113185486307827,
           "rotation": 0,
-          "target": "0-"
+          "target": "0-prihozaya"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "3--1",
-      "name": "Гостевая 1",
+      "id": "3-gostevaya1",
+      "name": "Gostevaya1",
       "levels": [
         {
           "tileSize": 256,
@@ -159,20 +159,20 @@ var APP_DATA = {
           "yaw": 1.6000853460232403,
           "pitch": -0.08717368388393254,
           "rotation": 0,
-          "target": "4--2"
+          "target": "4-gostevaya2"
         },
         {
           "yaw": -1.332801254017303,
           "pitch": -0.019681539880870957,
           "rotation": 0,
-          "target": "0-"
+          "target": "0-prihozaya"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "4--2",
-      "name": "Гостевая 2",
+      "id": "4-gostevaya2",
+      "name": "Gostevaya2",
       "levels": [
         {
           "tileSize": 256,
@@ -203,14 +203,14 @@ var APP_DATA = {
           "yaw": 3.098249030227267,
           "pitch": -0.0013558560117807872,
           "rotation": 0,
-          "target": "0-"
+          "target": "0-prihozaya"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "5--1",
-      "name": "Зал 1",
+      "id": "5-zal1",
+      "name": "Zal1",
       "levels": [
         {
           "tileSize": 256,
@@ -241,62 +241,62 @@ var APP_DATA = {
           "yaw": 2.689812944684137,
           "pitch": 0.04854887535223362,
           "rotation": 0,
-          "target": "0-"
+          "target": "0-prihozaya"
         },
         {
           "yaw": 2.9187406245248138,
           "pitch": 0.008327692881183069,
           "rotation": 0,
-          "target": "1--1"
+          "target": "1-dyshevaya1"
         },
         {
           "yaw": 3.080751204042347,
           "pitch": 0.09709734793745284,
           "rotation": 0,
-          "target": "3--1"
+          "target": "3-gostevaya1"
         },
         {
           "yaw": -0.6449834673355959,
           "pitch": 0.05881095507458056,
           "rotation": 0,
-          "target": "11-"
+          "target": "11-koridor"
         },
         {
           "yaw": 1.5361976430213478,
           "pitch": -0.06842582911106732,
           "rotation": 0,
-          "target": "9--1"
+          "target": "9-balcon1"
         },
         {
           "yaw": 1.0159552904892415,
           "pitch": -0.04489944155507075,
           "rotation": 0,
-          "target": "10--2"
+          "target": "10-balkon2"
         },
         {
           "yaw": 0.5690739687474675,
           "pitch": -0.14245407845290892,
           "rotation": 0,
-          "target": "6--2"
+          "target": "6-zal2"
         },
         {
           "yaw": 1.3223175193992383,
           "pitch": -0.075414721176438,
           "rotation": 0,
-          "target": "7--1"
+          "target": "7-kuhnya1"
         },
         {
           "yaw": 1.9251628770121405,
           "pitch": 0.009314274582955306,
           "rotation": 0,
-          "target": "8--2"
+          "target": "8-kuhnya2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "6--2",
-      "name": "Зал 2",
+      "id": "6-zal2",
+      "name": "Zal2",
       "levels": [
         {
           "tileSize": 256,
@@ -327,62 +327,62 @@ var APP_DATA = {
           "yaw": 2.0966902761452477,
           "pitch": 0.002616372380634502,
           "rotation": 0,
-          "target": "7--1"
+          "target": "7-kuhnya1"
         },
         {
           "yaw": 2.7224606146198873,
           "pitch": -0.07830451464598553,
           "rotation": 0,
-          "target": "8--2"
+          "target": "8-kuhnya2"
         },
         {
           "yaw": 2.345794730707242,
           "pitch": -0.020477248026935513,
           "rotation": 0,
-          "target": "9--1"
+          "target": "9-balcon1"
         },
         {
           "yaw": 1.6764441144649798,
           "pitch": 0.06823354420543382,
           "rotation": 0,
-          "target": "10--2"
+          "target": "10-balkon2"
         },
         {
           "yaw": -2.53891809791385,
           "pitch": -0.211898182143484,
           "rotation": 0,
-          "target": "0-"
+          "target": "0-prihozaya"
         },
         {
           "yaw": -2.502134410141103,
           "pitch": 0.04349544543995165,
           "rotation": 0,
-          "target": "1--1"
+          "target": "1-dyshevaya1"
         },
         {
           "yaw": -1.7305953669491991,
           "pitch": 0.05639035383476809,
           "rotation": 0,
-          "target": "5--1"
+          "target": "5-zal1"
         },
         {
           "yaw": -0.8058208980993715,
           "pitch": 0.07669676624474242,
           "rotation": 0,
-          "target": "11-"
+          "target": "11-koridor"
         },
         {
           "yaw": -2.2905387472185073,
           "pitch": 0.042255033057511326,
           "rotation": 0,
-          "target": "3--1"
+          "target": "3-gostevaya1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "7--1",
-      "name": "Кухня 1",
+      "id": "7-kuhnya1",
+      "name": "Kuhnya1",
       "levels": [
         {
           "tileSize": 256,
@@ -413,56 +413,56 @@ var APP_DATA = {
           "yaw": 1.425753904410442,
           "pitch": -0.1106543504473052,
           "rotation": 0,
-          "target": "8--2"
+          "target": "8-kuhnya2"
         },
         {
           "yaw": 0.6905967540199498,
           "pitch": -0.00029977166961714374,
           "rotation": 0,
-          "target": "9--1"
+          "target": "9-balcon1"
         },
         {
           "yaw": -0.4983621413375179,
           "pitch": -0.006130356162874051,
           "rotation": 0,
-          "target": "10--2"
+          "target": "10-balkon2"
         },
         {
           "yaw": -2.9235871159560567,
           "pitch": 0.03645670609926022,
           "rotation": 0,
-          "target": "5--1"
+          "target": "5-zal1"
         },
         {
           "yaw": -2.40499583840875,
           "pitch": 0.016231810023569437,
           "rotation": 0,
-          "target": "4--2"
+          "target": "4-gostevaya2"
         },
         {
           "yaw": -1.5114857736744032,
           "pitch": -0.0007429878193434547,
           "rotation": 0,
-          "target": "11-"
+          "target": "11-koridor"
         },
         {
           "yaw": 2.8387955173233212,
           "pitch": 0.03898336570372507,
           "rotation": 0,
-          "target": "3--1"
+          "target": "3-gostevaya1"
         },
         {
           "yaw": 2.791363395480949,
           "pitch": -0.2505962519851632,
           "rotation": 0,
-          "target": "0-"
+          "target": "0-prihozaya"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "8--2",
-      "name": "Кухня 2",
+      "id": "8-kuhnya2",
+      "name": "Kuhnya2",
       "levels": [
         {
           "tileSize": 256,
@@ -493,44 +493,44 @@ var APP_DATA = {
           "yaw": 0.7892062721746633,
           "pitch": -0.11137077053866307,
           "rotation": 0,
-          "target": "7--1"
+          "target": "7-kuhnya1"
         },
         {
           "yaw": 0.5188620699590807,
           "pitch": -0.05344431992455867,
           "rotation": 0,
-          "target": "10--2"
+          "target": "10-balkon2"
         },
         {
           "yaw": 1.4129579598484963,
           "pitch": -0.04893686069116043,
           "rotation": 0,
-          "target": "10--2"
+          "target": "10-balkon2"
         },
         {
           "yaw": -0.2965095657598802,
           "pitch": -0.008502508614418147,
           "rotation": 0,
-          "target": "6--2"
+          "target": "6-zal2"
         },
         {
           "yaw": -1.0713362855743895,
           "pitch": 0.012169871580713476,
           "rotation": 0,
-          "target": "5--1"
+          "target": "5-zal1"
         },
         {
           "yaw": -0.7758463490664518,
           "pitch": 0.011705589350768264,
           "rotation": 0,
-          "target": "11-"
+          "target": "11-koridor"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "9--1",
-      "name": "Балкон 1",
+      "id": "9-balcon1",
+      "name": "Balcon1",
       "levels": [
         {
           "tileSize": 256,
@@ -561,14 +561,14 @@ var APP_DATA = {
           "yaw": -0.4290796657336742,
           "pitch": 0.1516405623611341,
           "rotation": 0,
-          "target": "8--2"
+          "target": "8-kuhnya2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "10--2",
-      "name": "Балкон 2",
+      "id": "10-balkon2",
+      "name": "Balkon2",
       "levels": [
         {
           "tileSize": 256,
@@ -599,14 +599,14 @@ var APP_DATA = {
           "yaw": 1.3143961446752037,
           "pitch": 0.16685374559530786,
           "rotation": 0,
-          "target": "6--2"
+          "target": "6-zal2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "11-",
-      "name": "Коридор",
+      "id": "11-koridor",
+      "name": "Koridor",
       "levels": [
         {
           "tileSize": 256,
@@ -637,50 +637,50 @@ var APP_DATA = {
           "yaw": -0.8079865308064242,
           "pitch": 0.08073246080435936,
           "rotation": 0,
-          "target": "5--1"
+          "target": "5-zal1"
         },
         {
           "yaw": 0.6101200893230025,
           "pitch": 0.09092670152153204,
           "rotation": 0,
-          "target": "14-"
+          "target": "14-garderobnaya"
         },
         {
           "yaw": 1.1899612670051063,
           "pitch": 0.0602002422996879,
           "rotation": 0,
-          "target": "17---1"
+          "target": "17-spalnyaroditeley1"
         },
         {
           "yaw": 1.4635402576528609,
           "pitch": 0.06553832724459596,
           "rotation": 0,
-          "target": "16---"
+          "target": "16-spalnyamladsheydocheri"
         },
         {
           "yaw": 1.9117211286492157,
           "pitch": 0.09808827119499064,
           "rotation": 0,
-          "target": "13-"
+          "target": "13-postirochnaya"
         },
         {
           "yaw": -2.6168585729854534,
           "pitch": 0.07388477590444431,
           "rotation": 0,
-          "target": "12-"
+          "target": "12-vannaya"
         },
         {
           "yaw": -1.5845454356794875,
           "pitch": 0.17110327122922797,
           "rotation": 0,
-          "target": "15---"
+          "target": "15-spalnyastarsheydocheri"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "12-",
-      "name": "Ванная",
+      "id": "12-vannaya",
+      "name": "Vannaya",
       "levels": [
         {
           "tileSize": 256,
@@ -711,14 +711,14 @@ var APP_DATA = {
           "yaw": -1.3391853555968023,
           "pitch": -0.2568545255485972,
           "rotation": 0,
-          "target": "11-"
+          "target": "11-koridor"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "13-",
-      "name": "Постирочная",
+      "id": "13-postirochnaya",
+      "name": "Postirochnaya",
       "levels": [
         {
           "tileSize": 256,
@@ -749,14 +749,14 @@ var APP_DATA = {
           "yaw": -1.6488053321167602,
           "pitch": -0.03685067177530854,
           "rotation": 0,
-          "target": "11-"
+          "target": "11-koridor"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "14-",
-      "name": "Гардеробная",
+      "id": "14-garderobnaya",
+      "name": "Garderobnaya",
       "levels": [
         {
           "tileSize": 256,
@@ -787,14 +787,14 @@ var APP_DATA = {
           "yaw": -1.7249122290796848,
           "pitch": 0.09420326736804085,
           "rotation": 0,
-          "target": "11-"
+          "target": "11-koridor"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "15---",
-      "name": "Спальня Старшей Дочери",
+      "id": "15-spalnyastarsheydocheri",
+      "name": "SpalnyaStarsheyDocheri",
       "levels": [
         {
           "tileSize": 256,
@@ -825,14 +825,14 @@ var APP_DATA = {
           "yaw": -0.3167079357511202,
           "pitch": 0.054132032782682415,
           "rotation": 0,
-          "target": "11-"
+          "target": "11-koridor"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "16---",
-      "name": "Спальня Младей Дочери",
+      "id": "16-spalnyamladsheydocheri",
+      "name": "SpalnyaMladsheyDocheri",
       "levels": [
         {
           "tileSize": 256,
@@ -863,14 +863,14 @@ var APP_DATA = {
           "yaw": -1.828370903406423,
           "pitch": 0.019554263156971174,
           "rotation": 0,
-          "target": "11-"
+          "target": "11-koridor"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "17---1",
-      "name": "Спальня Родителей 1",
+      "id": "17-spalnyaroditeley1",
+      "name": "SpalnyaRoditeley1",
       "levels": [
         {
           "tileSize": 256,
@@ -901,20 +901,20 @@ var APP_DATA = {
           "yaw": 3.130832695699776,
           "pitch": -0.06497199203953663,
           "rotation": 0,
-          "target": "18---2"
+          "target": "18-spalnyaroditeley2"
         },
         {
           "yaw": -0.2734410895437964,
           "pitch": -0.049764434731592644,
           "rotation": 0,
-          "target": "11-"
+          "target": "11-koridor"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "18---2",
-      "name": "пальня Родителей 2",
+      "id": "18-spalnyaroditeley2",
+      "name": "SpalnyaRoditeley2",
       "levels": [
         {
           "tileSize": 256,
@@ -945,13 +945,13 @@ var APP_DATA = {
           "yaw": 2.313551653880161,
           "pitch": -0.006001833559041714,
           "rotation": 0,
-          "target": "11-"
+          "target": "11-koridor"
         },
         {
           "yaw": -3.0747375138500335,
           "pitch": 0.021289902402436667,
           "rotation": 0,
-          "target": "17---1"
+          "target": "17-spalnyaroditeley1"
         }
       ],
       "infoHotspots": []
